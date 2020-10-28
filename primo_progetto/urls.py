d4c37de218f8f8becab9c96d24c92194e7e37a06
diegoarduca11 <71756434+diegoarduca11@users.noreply.github.com>
2020-10-28 13:12:33 +0100

    Update urls.py

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from prima_app.views import index
+from seconda_app.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
