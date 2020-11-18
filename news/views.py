10e282b8ed78208ec194c407950297d147053ba5
diegoarduca11 <71756434+diegoarduca11@users.noreply.github.com>
2020-11-18 13:53:15 +0100

    ...

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Articolo, Giornalista
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 # Create your views here.
 def home(request):
    articoli = Articolo.objects.all()
@@ -19,3 +20,12 @@ def articoloDetailView(request, pk):
 class ArticoloDetailViewCB(DetailView):
     model = Articolo
     template_name = "articolo_detail.html"
+
+class ArticoloListView(ListView):
+    model = Articolo
+    template_name = "lista_articoli.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["articoli"] = Articolo.objects.all()
+        return context
