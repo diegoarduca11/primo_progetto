f8957db8edd25b27e0e56ec90ac006391e8879ae
diegoarduca11 <71756434+diegoarduca11@users.noreply.github.com>
2020-11-25 13:14:27 +0100

    ...

--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,5 @@ urlpatterns= [
     #path("articoli//<int:pk>", articoloDetailView, name="articolo_detail")
     path("articoli/<int:pk>", ArticoloDetailViewCB.as_view(), name="articolo_detail"),
     path("lista_articoli/", ArticoloListView.as_view(), name="lista_articoli")
+    
 ]
